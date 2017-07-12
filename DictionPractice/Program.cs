5467491cded82b6b1762eb9c19440b7bf217eee2
pfittante <pfittante@yahoo.com>
2017-07-12 17:06:39 -0400

    Dictionary practice

--- DictionPractice/Program.cs
+++ DictionPractice/Program.cs
@@ -10,33 +10,101 @@ namespace DictionPractice
     {
         static void Main(string[] args)
         {
-            Dictionary<string, string> favCandy = new Dictionary<string, string>()//instansiaite and 
+            //Dictionary<string, string> favCandy = new Dictionary<string, string>()//instansiaite and 
+            //{
+            //    {"A1", "twix" },
+            //    {"A2", "Reeses" },
+            //    {"A3", "M&M" }
+            //};
+
+            //foreach (KeyValuePair<string,string>  candy in favCandy){
+
+            //    Console.WriteLine(candy);
+            //}
+
+            //Dictionary<int, string> drinkMachine = new Dictionary<int, string>();
+            //drinkMachine.Add(10, "ginger ale");
+            //drinkMachine.Add(11, "pepsi");
+            //drinkMachine.Add(9, "dr. pepper");
+
+            //drinkMachine.Count();// tells how many items in the dictionary
+
+            //foreach (KeyValuePair<int, string> drink in drinkMachine)
+            //{
+            //    Console.WriteLine(drink.Key);
+            //    Console.WriteLine(drink.Value);
+            //}
+
+            //Dictionary<int, String> coatCheck = new Dictionary<int, string>();
+
+            //coatCheck.Add(1, "leather");
+            //coatCheck.Add(2, "suade");
+            //coatCheck.Add(3, "biker leather");
+            //coatCheck.Add(4, "stripper coat");
+            //coatCheck.Add(5, "winter coat");
+
+            //foreach(KeyValuePair<int, string> coat in coatCheck)
+            //{
+            //    Console.WriteLine(coat);
+            //}
+
+            Dictionary<string, int> aminmalCount = new Dictionary<string, int>();
+
+            aminmalCount.Add("snake", 1);
+            aminmalCount.Add("parrot", 2);
+            aminmalCount.Add("wolf", 3);
+            aminmalCount.Add("monkey", 4);
+            aminmalCount.Add("giraffe", 5);
+            aminmalCount.Add("bear", 6);
+            aminmalCount.Add("spider", 7);
+            aminmalCount.Add("deer", 8);
+
+            int tempHigh = 1;
+            int tempLow = 1;
+            string tempKeylow = "";
+            string tempKeyhigh = " ";
+
+            foreach (KeyValuePair<string, int> count in aminmalCount)
             {
-                {"A1", "twix" },
-                {"A2", "Reeses" },
-                {"A3", "M&M" }
-            };
+                if (count.Value <= tempLow)
+                {
 
-            foreach (KeyValuePair<string,string>  candy in favCandy){
+                    tempKeylow = count.Key;
+                    tempLow = count.Value;
 
-                Console.WriteLine(candy);
+                }
+                if (count.Value > tempLow)
+                {
+                    tempKeyhigh = count.Key;
+                    tempHigh = count.Value;
+
+
+
+                }
+
+            }
+            Console.WriteLine(tempKeylow);
+            Console.WriteLine(tempKeyhigh);
+
+            foreach (KeyValuePair<string, int> count in aminmalCount)
+            {
+                if (tempKeylow == count.Key)
+                {
+
+
+                    aminmalCount.Remove(count.Key);
+                    break;
+                }
+
             }
 
-            Dictionary<int, string> drinkMachine = new Dictionary<int, string>();
-            drinkMachine.Add(10, "ginger ale");
-            drinkMachine.Add(11, "pepsi");
-            drinkMachine.Add(9, "dr. pepper");
-
-            drinkMachine.Count();// tells how many items in the dictionary
-
-            foreach (KeyValuePair<int, string> drink in drinkMachine)
+            foreach (KeyValuePair<string, int> count in aminmalCount)
             {
-                Console.WriteLine(drink.Key);
-                Console.WriteLine(drink.Value);
+                Console.WriteLine(count);
+
             }
-        }   
 
-            
-
+        }
     }
 }
+    
