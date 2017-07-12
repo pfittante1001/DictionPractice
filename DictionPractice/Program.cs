f51b3fde6094bd7fe53b2f5cc61d2f65c316c44b
pfittante <pfittante@yahoo.com>
2017-07-12 14:17:54 -0400

    Dictionary practice

--- DictionPractice/Program.cs
+++ DictionPractice/Program.cs
@@ -10,6 +10,33 @@ namespace DictionPractice
     {
         static void Main(string[] args)
         {
-        }
+            Dictionary<string, string> favCandy = new Dictionary<string, string>()//instansiaite and 
+            {
+                {"A1", "twix" },
+                {"A2", "Reeses" },
+                {"A3", "M&M" }
+            };
+
+            foreach (KeyValuePair<string,string>  candy in favCandy){
+
+                Console.WriteLine(candy);
+            }
+
+            Dictionary<int, string> drinkMachine = new Dictionary<int, string>();
+            drinkMachine.Add(10, "ginger ale");
+            drinkMachine.Add(11, "pepsi");
+            drinkMachine.Add(9, "dr. pepper");
+
+            drinkMachine.Count();// tells how many items in the dictionary
+
+            foreach (KeyValuePair<int, string> drink in drinkMachine)
+            {
+                Console.WriteLine(drink.Key);
+                Console.WriteLine(drink.Value);
+            }
+        }   
+
+            
+
     }
 }
