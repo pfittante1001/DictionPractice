48509f7008fe43a643092efb2a91135d888a2fee
pfittante <pfittante@yahoo.com>
2017-07-13 00:38:23 -0400

    Dictionary practice

--- DictionPractice/Program.cs
+++ DictionPractice/Program.cs
@@ -48,63 +48,155 @@ namespace DictionPractice
             //    Console.WriteLine(coat);
             //}
 
+
+
+            bool exitProgram = false;
+            bool enterAnother = false;
+            bool removeAnother = false;
+            int tempMin = 1;
+            int tempMax;
+            List<string> tempKeyLowLST = new List<string>();
+            List<string> tempKeyHighLST = new List<string>();
             Dictionary<string, int> aminmalCount = new Dictionary<string, int>();
 
-            aminmalCount.Add("snake", 1);
-            aminmalCount.Add("parrot", 2);
-            aminmalCount.Add("wolf", 3);
-            aminmalCount.Add("monkey", 4);
-            aminmalCount.Add("giraffe", 5);
-            aminmalCount.Add("bear", 6);
-            aminmalCount.Add("spider", 7);
-            aminmalCount.Add("deer", 8);
+            aminmalCount.Add("SNAKE", 9);
+            aminmalCount.Add("PARROT", 1);
+            aminmalCount.Add("WOLF", 3);
+            aminmalCount.Add("MONKEY", 4);
+            aminmalCount.Add("GIRAFFE", 1);
+            aminmalCount.Add("BEAR", 8);
+            aminmalCount.Add("SPIDER", 7);
+            aminmalCount.Add("DEER", 8);
+            aminmalCount.Add("BULL", 5);
+            aminmalCount.Add("LLAMA", 3);
 
-            int tempHigh = 1;
-            int tempLow = 1;
-            string tempKeylow = "";
-            string tempKeyhigh = " ";
 
-            foreach (KeyValuePair<string, int> count in aminmalCount)
+            //Console.Write("            List of animals before removal:\n\n");
+            //foreach (KeyValuePair<string, int> count in aminmalCount)
+            //{
+            //    Console.Write("{0} ", count.Key);
+            //}
+            Console.WriteLine("\n\n\n");
+            //This foreach loop finds the min and max values. The values are saved in two variables, and the keys
+            //are saved in to lists
+            while (exitProgram == false)
             {
-                if (count.Value <= tempLow)
+                Console.Clear();
+                Console.WriteLine("      If you would like to enter a new animal, please enter the name first");
+                Console.WriteLine("             then the total number of the animal you entered\n\n\n\n\n");
+                while (enterAnother == false)
                 {
+                    Console.Write("\n\nPlease enter the animal name or quit to exit: ");
+                    string keyInputStr = Console.ReadLine().ToUpper();
+                    ExitProgram(keyInputStr);
 
-                    tempKeylow = count.Key;
-                    tempLow = count.Value;
+                    Console.Write("\n\nPlease enter the amount of the animal or quit to exit: ");
+                    string valueInputStr = Console.ReadLine().ToUpper();
+                    ExitProgram(valueInputStr);
+                    int valueInput = int.Parse(valueInputStr);
+
+                    aminmalCount.Add(keyInputStr, valueInput);
+                    Console.Clear();
+                    Console.Write("\n\n\nSelect enter to add another animal or quit to continue: ");
+                    string enterAnotherStr = Console.ReadLine().ToUpper();
+                    if(enterAnotherStr == "QUIT")
+                    {
+                        enterAnother = true;
+                    }
+                }
+
+                Console.Clear();
+                Console.WriteLine("      Select enter to remove an animal name or quit to continue:");
+                while (removeAnother == false)
+                {
+                    string removeAnotherStr = Console.ReadLine().ToUpper();
+                    if (removeAnotherStr == "QUIT")
+                    {
+                        removeAnother = true;
+                        break;
+                    }
+                    Console.Write("\n\nPlease enter the animal name to remove or quit to exit: ");
+                    string keyInputStr = Console.ReadLine().ToUpper();
+                    ExitProgram(keyInputStr);
+
+                    aminmalCount.Remove(keyInputStr);
+                    Console.Clear();
+                    Console.Write("\n\n\nSelect enter to remove another animal or quit to continue: ");
+                    removeAnotherStr = Console.ReadLine().ToUpper();
+                    if (removeAnotherStr == "QUIT")
+                    {
+                        removeAnother = true;
+                    }
+                }
+
+                
+
+                tempMax = aminmalCount.Values.Max();
+                foreach (KeyValuePair<string, int> count in aminmalCount)
+                {
+                    if (count.Value <= tempMin)
+                    {
+                        tempMin = count.Value;
+                        tempKeyLowLST.Add(count.Key);
+                    }
+
+                    if (tempMax == count.Value && tempKeyHighLST.Contains(count.Key)==false)
+                    {
+                        tempKeyHighLST.Add(count.Key);
+                    }
 
                 }
-                if (count.Value > tempLow)
+
+                //These nested loops compare the keys in the tempKeyLowLST and the animalCount dict. If the keys match
+                // the entry is removed from animalCount
+                for (int i = 0; i < tempKeyLowLST.Count; i++)
                 {
-                    tempKeyhigh = count.Key;
-                    tempHigh = count.Value;
+                    foreach (KeyValuePair<string, int> count in aminmalCount)
+                    {
+                        if (tempKeyLowLST[i] == count.Key)
+                        {
+                            aminmalCount.Remove(count.Key);
+                            break;
+                        }
+                    }
+                }
 
-
+                Console.Clear();
+                Console.Write("\n\n\nThe animals with the most numbers each are: ");
+                for (int i = 0; i < tempKeyHighLST.Count; i++)
+                {
+                    Console.Write("{0} ", tempKeyHighLST[i]);
 
                 }
 
-            }
-            Console.WriteLine(tempKeylow);
-            Console.WriteLine(tempKeyhigh);
-
-            foreach (KeyValuePair<string, int> count in aminmalCount)
-            {
-                if (tempKeylow == count.Key)
+                Console.Write(" \n\n\n\n\n           List of animals after removal:\n\n");
+                foreach (KeyValuePair<string, int> count in aminmalCount)
                 {
-
-
-                    aminmalCount.Remove(count.Key);
-                    break;
+                    Console.Write("{0} ", count.Key);
                 }
 
+                Console.Write("\n\n\n\nWould you like to enter another animal enter to continue or quit to exit: ");
+                string exitProgramStr = Console.ReadLine().ToUpper();
+                if (exitProgramStr == "QUIT")
+                {
+                    exitProgram = true;
+
+                }else
+                {
+                    enterAnother = false;
+                    removeAnother = false;
+                }
             }
-
-            foreach (KeyValuePair<string, int> count in aminmalCount)
-            {
-                Console.WriteLine(count);
-
-            }
-
         }
+
+        public static void ExitProgram(string exitProgram) 
+        {
+            if(exitProgram == "QUIT")
+            {
+                System.Environment.Exit(1);
+            }
+        }
+       
     }
 }
     
